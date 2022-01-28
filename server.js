5c559b786f83e9ba87f0a051dfd4bae240b184b2
Michelle Wallis <michellewallis89@gmail.com>
2022-01-28 13:03:35 +0100

    creation model schema

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ dotenv.config({ path: './.env' });
 
 const app = require('./app')
 
+//DATA BASE CONECTION
+const DB = process.env.DATABASE;
+mongoose.connect(DB, {
+    USEnEWUrlParser: true,
+    useUnifiedTopology : true
+})
+.then(( )=> console.log('DB Connection Sucefull'))
+
 // SERVER
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
