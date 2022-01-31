9791128221dee8a7401f16b525b21009d58f6d2d
Michelle Wallis <michellewallis89@gmail.com>
2022-01-31 14:44:16 +0100

    models and controllers

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const tourRoutes = require('./routes/tourRoutes')
 
 const app = express();
 
+// MIDDLEWARES
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 //ROUTES 
 app.use('/api/v1/tours', tourRoutes)
 
