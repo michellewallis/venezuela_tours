e61f83ebd031e631371be60d13ba78a9b0b92826
Michelle Wallis <michellewallis89@gmail.com>
2022-02-01 10:00:47 +0100

    add filters, sort

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,25 +1,68 @@
 const Tour = require('../models/tourModel');
 
 
-//ge all tours
+//Get all tours
 exports.getAllTours = async (req, res) => {
     try{
-        const tours = await Tour.find();
-    res.status(200).json({
-        status: 'succes',
-        results: tours.length,
-        data: { tours: tours},
-    });
-}catch (error){
-    console.log(error);
-    res.status(400).json({
-        status : 'fail',
-        message : 'error',
-    })
-}
-};
+        // BUILD QUERY
+        //1A filtering
+        //page, sort, limit, fields
 
-// get one tour by id
+        // create a req.query copy
+        const queryObj = {...req.query}
+        const excludeFields = ['page', 'sort', 'limit','fields']
+        excludeFields.forEach(field => delete queryObj [field]);
+
+
+    // 1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`); // Regular expresion
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sort
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('price');
+    }
+
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 4) Pagination
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 2;
+    const skip = (page - 1) * limit;
+
+    query = query.limit(limit).skip(skip);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
+    // EXECUTE QUERY
+    const tours = await query;
+    res.status(200).json({
+      status: 'succes',
+      results: tours.length,
+      data: { tours: tours },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
+// Get tour by id
 exports.getTour = async (req,res)=> {
     try{
         const tour = await Tour.findById(req.params.id);
---
+++ middlewares/aliasTopTours.js
@@ -0,0 +1,9 @@
+exports.aliasTopTours = (req, res, next) => {
+    req.query.limit='2';
+    req.query.sort = 'price';
+    req.query.fields = 'name, price, ratingsAverage,sumary, dificulty';
+
+
+
+    next ()
+}
